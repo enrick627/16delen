bd2c40fa4903b9a2f4b87aa7773e5940165d56ad
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2019-11-13 08:54:58 +0100

    Add project files.

---
+++ delen/Program.cs
@@ -0,0 +1,29 @@
+﻿// Enrick De Munter 5ITN nr4
+/*we willen dat alle getallen deelbaar zijn
+ * 
+ * input
+ * Vraag het deeltal aan de gebruiker
+ * Vraag het getal dat je wil laten delen door het deeltal
+ * 
+ * processing
+ * Doe de deling
+ * 
+ * 
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
